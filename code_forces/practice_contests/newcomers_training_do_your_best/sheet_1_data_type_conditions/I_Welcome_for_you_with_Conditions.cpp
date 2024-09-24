075143f08e97b80689a68e9bb2d9b5478a7c23ab
TanvirFahimBD <tanvirfahim6939@gmail.com>
2024-09-24 21:28:37 +0600

    I. Welcome for you with Conditions solved newcomers training sheet 1 in cf

---
+++ code_forces/practice_contests/newcomers_training_do_your_best/sheet_1_data_type_conditions/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,21 @@
+//I. Welcome for you with Conditions - https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2;
+
+    cin >> num1 >> num2;
+
+    if(num1>=num2){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
